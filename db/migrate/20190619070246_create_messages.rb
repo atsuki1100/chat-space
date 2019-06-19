2294476e353bbc09734edc594668cf7d686b1be3
atsuki1100 <atuki0303@outlook.jp>
2019-06-19 17:24:02 +0900

    Create 20190619070246_create_messages.rb

---
+++ db/migrate/20190619070246_create_messages.rb
@@ -0,0 +1,11 @@
+class CreateMessages < ActiveRecord::Migration[5.0]
+  def change
+    create_table :messages do |t|
+      t.text :body
+      t.string :image
+      t.integer :group_id, null: false
+      t.integer :user_id, null: false
+      t.timestamps null: false
+    end
+  end
+end
